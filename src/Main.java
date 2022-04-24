faab568ce5bf905cd4e4b93c9ab54c279774adad
RuzaikinDY <Devilwind@mail.ru>
2022-04-24 09:10:06 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket=100;
+        int bonus= ticket/20;
+
+        System.out.println("Бонусные мили:" + bonus);
+    }
+
+}
